9bc3d9eec5010652cd54466c6a7be7bddb47511c
rajarya199 <aryalrajan078@gmail.com>
2024-05-17 19:57:04 +0545

    register form

--- clientside/src/pages/Register.jsx
+++ clientside/src/pages/Register.jsx
@@ -14,11 +14,53 @@ const Register = () => {
      />
     <input type="email"
      placeholder="your@email.com"
-    
     />
+      <input 
+              type="text" 
+              placeholder='Phone number' 
+            />
+             {/* <select 
+              className="block w-full mb-2 p-2 border rounded" 
+              placeholder='Gender'>
+              <option value="" disabled selected>Select your gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select> */}
+             <div className="  flex justify-evenly mb-2 mt-2 ">
+               <span className="block mb-1 mr-10">Gender:</span>  
+              <label className='mr-8' >
+                <input 
+                  type="radio" 
+                  name="gender" 
+                  value="male" 
+                  className="mr-2"
+                />
+                Male
+              </label>
+              <label className="mr-8">
+                <input 
+                  type="radio" 
+                  name="gender" 
+                  value="female" 
+                  className="mr-2"
+                />
+                Female
+              </label>
+              <label className="mr-8">
+                <input 
+                  type="radio" 
+                  name="gender" 
+                  value="other" 
+                  className="mr-2"
+                />
+                Other
+              </label>
+            </div>
     <input type="password" 
     placeholder='password' 
     />
+
     <button className='primary primary p-2 w-full text-white rounded-2xl bg-slate-400'>Register</button>
     <div className='text-center py-2 text-gray-500'>
         Already a member? &nbsp;&nbsp;
