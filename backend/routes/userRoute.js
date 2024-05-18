b60db7b9109347abad7d7858ca4be4059966ee53
rajarya199 <aryalrajan078@gmail.com>
2024-05-18 13:51:33 +0545

    validate user

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,9 @@
 const express=require('express')
 const { postUser, postEmailConfirmation, signIn, signOut } = require('../controllers/userController')
+const { userValidation, validation, passwordValidation } = require('../validation/validator')
 const router=express.Router()
 
-router.post('/register',postUser)
+router.post('/register',userValidation,passwordValidation,validation,postUser)
 router.put('/confirmation/:token',postEmailConfirmation)
 router.post('/signin',signIn)
 router.post('/signout',signOut)
